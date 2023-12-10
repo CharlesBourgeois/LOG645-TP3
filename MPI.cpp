819b3adda6578b0728d935439a18f20c1c74f250
Charles Bourgeois <72451393+CharlesBourgeois@users.noreply.github.com>
2023-12-09 22:17:57 -0500

    Update MPI.cpp

--- MPI.cpp
+++ MPI.cpp
@@ -123,6 +123,131 @@ void processReceivedAnimals(Animal* buffer, int numAnimals) {
     }
 }
 
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <cmath>
+
+#define OCEAN_SIZE 100
+#define MREQUIN 10
+#define MPOISSON 5
+#define PREP 0.1
+#define EMPTY -1
+#define MAX_ANIMALS 1000
+
+typedef struct {
+    int type; // 0 pour poisson, 1 pour requin
+    float x, y; // Position
+    float vx, vy; // Vitesse
+    float ax, ay; // Accélération
+    int hunger; // Faim pour les requins
+} Animal;
+
+Animal ocean[MAX_ANIMALS];
+
+void initializeOcean() {
+    srand(time(NULL));
+    for (int i = 0; i < MAX_ANIMALS; i++) {
+        ocean[i].type = rand() % 2;
+        ocean[i].x = rand() % OCEAN_SIZE;
+        ocean[i].y = rand() % OCEAN_SIZE;
+        ocean[i].vx = ocean[i].vy = 0;
+        ocean[i].ax = ocean[i].ay = 0;
+        ocean[i].hunger = 0;
+    }
+}
+
+void updateForces(Animal* a) {
+    float force_x = 0.0, force_y = 0.0;
+
+    force_x += 0.1; 
+
+    for (int i = 0; i < MAX_ANIMALS; i++) {
+        if (ocean[i].type != a->type) {
+            float distance = sqrt(pow(ocean[i].x - a->x, 2) + pow(ocean[i].y - a->y, 2));
+            if (a->type == 0 && distance < 10) {
+                force_x -= 1 / distance; 
+            }
+            if (a->type == 1 && distance < 20) { 
+                force_x += 1 / distance; 
+            }
+        }
+    }
+
+    a->ax = force_x / (a->type == 0 ? MPOISSON : MREQUIN);
+    a->ay = force_y / (a->type == 0 ? MPOISSON : MREQUIN);
+}
+
+void handleCollisionsAndReproduction() {
+    for (int i = 0; i < MAX_ANIMALS; i++) {
+        for (int j = i + 1; j < MAX_ANIMALS; j++) {
+            float distance = sqrt(pow(ocean[i].x - ocean[j].x, 2) + pow(ocean[i].y - ocean[j].y, 2));
+            if (distance < 1.0) { 
+                if (ocean[i].type == ocean[j].type) {
+                    float temp_vx = ocean[i].vx;
+                    float temp_vy = ocean[i].vy;
+                    ocean[i].vx = ocean[j].vx;
+                    ocean[i].vy = ocean[j].vy;
+                    ocean[j].vx = temp_vx;
+                    ocean[j].vy = temp_vy;
+                }
+                else {
+                    if (ocean[i].type == 1) ocean[j].type = EMPTY; 
+                    else ocean[i].type = EMPTY;
+                }
+            }
+        }
+        if (rand() < PREP * RAND_MAX) { 
+            for (int k = 0; k < MAX_ANIMALS; k++) {
+                if (ocean[k].type == EMPTY) {
+                    ocean[k].type = ocean[i].type;
+                    ocean[k].x = ocean[i].x + ((rand() % 3) - 1);
+                    ocean[k].y = ocean[i].y + ((rand() % 3) - 1);
+                    ocean[k].vx = ocean[k].vy = 0;
+                    ocean[k].ax = ocean[k].ay = 0;
+                    ocean[k].hunger = 0;
+                    break;
+                }
+            }
+        }
+    }
+}
+
+void updatePosition(Animal* a, float timeStep) {
+    a->vx += a->ax * timeStep;
+    a->vy += a->ay * timeStep;
+    a->x += a->vx * timeStep;
+    a->y += a->vy * timeStep;
+}
+
+void exchangeAnimals(int world_rank, int world_size, int count, Animal* buffer, int* num_received) {
+    MPI_Status status;
+
+    if (world_size > 1) {
+        if (world_rank == 0) {
+            MPI_Send(buffer, count * sizeof(Animal), MPI_BYTE, 1, 0, MPI_COMM_WORLD);
+            MPI_Probe(1, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+            MPI_Get_count(&status, MPI_BYTE, num_received);
+            MPI_Recv(buffer, *num_received, MPI_BYTE, 1, 0, MPI_COMM_WORLD, &status);
+        } else if (world_rank == 1) {
+            MPI_Recv(buffer, MAX_ANIMALS * sizeof(Animal), MPI_BYTE, 0, 0, MPI_COMM_WORLD, &status);
+            MPI_Send(buffer, count * sizeof(Animal), MPI_BYTE, 0, 0, MPI_COMM_WORLD);
+        }
+    }
+}
+
+void processReceivedAnimals(Animal* buffer, int numAnimals) {
+    for (int i = 0; i < numAnimals; i++) {
+        for (int j = 0; j < MAX_ANIMALS; j++) {
+            if (ocean[j].type == EMPTY) {
+                ocean[j] = buffer[i];
+                break;
+            }
+        }
+    }
+}
+
 void printOcean(Animal ocean[], int oceanSize) {
     char displayGrid[oceanSize][oceanSize];
 
@@ -198,6 +323,104 @@ int main(int argc, char** argv) {
         int numAnimalsReceived = (world_rank == 0) ? num_received / sizeof(Animal) : count;
         processReceivedAnimals(buffer, numAnimalsReceived);
         printOcean(ocean, OCEAN_SIZE);
+    }
+
+    MPI_Finalize();
+    return 0;
+}
+
+void drawOcean(Animal ocean[], int oceanSize) {
+    // Create the main window
+    sf::RenderWindow window(sf::VideoMode(800, 800), "Ocean Simulation");
+    
+    // Define the size of each cell based on the window size and ocean size
+    float cellWidth = static_cast<float>(window.getSize().x) / oceanSize;
+    float cellHeight = static_cast<float>(window.getSize().y) / oceanSize;
+
+    // Start the window's event loop
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        // Clear the screen with a blue color to represent water
+        window.clear(sf::Color::Blue);
+
+        // Draw the ocean creatures
+        for (int i = 0; i < MAX_ANIMALS; i++) {
+            if (ocean[i].type != EMPTY) {
+                sf::RectangleShape cell(sf::Vector2f(cellWidth, cellHeight));
+                cell.setPosition(ocean[i].x * cellWidth, ocean[i].y * cellHeight);
+
+                // Color the cells based on the type
+                if (ocean[i].type == 0) { // Fish
+                    cell.setFillColor(sf::Color::Green);
+                } else if (ocean[i].type == 1) { // Shark
+                    cell.setFillColor(sf::Color::Red);
+                }
+
+                window.draw(cell);
+            }
+        }
+
+        // Update the window
+        window.display();
+
+        // Break the loop to allow for the next simulation step
+        break;
+    }
+}
+
+int main(int argc, char** argv) {
+
+    MPI_Init(&argc, &argv);
+
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    // Now you can use world_size and world_rank
+    int domain_size = OCEAN_SIZE / sqrt(world_size);
+    int start_x = (world_rank % (int)sqrt(world_size)) * domain_size;
+    int start_y = (world_rank / (int)sqrt(world_size)) * domain_size;
+
+    int num_received = 0;
+
+    initializeOcean();
+
+    float timeStep = 1.0;
+    int count = 0;
+    Animal buffer[MAX_ANIMALS];
+
+    for (int step = 0; step < 1000; step++) {
+        count = 0;
+        for (int i = 0; i < MAX_ANIMALS; i++) {
+            if (ocean[i].x >= start_x && ocean[i].x < start_x + domain_size &&
+                ocean[i].y >= start_y && ocean[i].y < start_y + domain_size) {
+                updateForces(&ocean[i]);
+                updatePosition(&ocean[i], timeStep);
+
+            }
+            if (ocean[i].x < start_x || ocean[i].x >= start_x + domain_size ||
+                ocean[i].y < start_y || ocean[i].y >= start_y + domain_size) {
+                buffer[count++] = ocean[i];
+                ocean[i].type = EMPTY;
+            }
+        }
+
+        handleCollisionsAndReproduction();
+
+        MPI_Barrier(MPI_COMM_WORLD);
+        exchangeAnimals(world_rank, world_size, count, buffer, &num_received);
+        MPI_Barrier(MPI_COMM_WORLD);
+
+        int numAnimalsReceived = (world_rank == 0) ? num_received / sizeof(Animal) : count;
+        processReceivedAnimals(buffer, numAnimalsReceived);
+        drawOcean(ocean, OCEAN_SIZE);
     }
 
     MPI_Finalize();
