1153bea6d945e47c1159c072f466f8616ac7c7c4
Charles Bourgeois <72451393+CharlesBourgeois@users.noreply.github.com>
2023-12-15 16:36:41 -0500

    Update MPI.cpp

--- MPI.cpp
+++ MPI.cpp
@@ -59,7 +59,7 @@ void updateForces(Animal* a, Animal* local_ocean, int local_count) {
     for (int i = 0; i < local_count; i++) {
         if (a == &local_ocean[i]) continue;
         float distance = sqrt(pow(local_ocean[i].x - a->x, 2) + pow(local_ocean[i].y - a->y, 2));
-        if (distance == 0) continue;
+        if (distance < EPSILON) continue;
         
         if (a->type == 1 && local_ocean[i].type == 0) {
             if (distance < VISIBILITY_RANGE) {
