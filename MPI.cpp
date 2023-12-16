331da7924e262a890176933d7a67bce599263267
Charles Bourgeois <72451393+CharlesBourgeois@users.noreply.github.com>
2023-12-15 21:59:50 -0500

    Update MPI.cpp

--- MPI.cpp
+++ MPI.cpp
@@ -6,12 +6,12 @@
 #include <unistd.h>
 #include <cfloat>
 
-#define OCEAN_SIZE 48
+#define OCEAN_SIZE 50
 #define MREQUIN 10
 #define MPOISSON 5
 #define PREP 0.1
 #define EMPTY -1
-#define MAX_ANIMALS 500
+#define MAX_ANIMALS 250
 #define HUNGER_LIMIT 5
 
 #define REPPOISSON 1.0         
