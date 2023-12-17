01a820d504af33b167e67492e104872139845286
JustinLcx <38329401+Justinlcx@users.noreply.github.com>
2023-12-17 13:49:50 -0500

    ds

--- Séquentielle/MPI.cpp
+++ Séquentielle/MPI.cpp
@@ -8,21 +8,18 @@
 #include <chrono>
 #include <iostream>
 
-#define OCEAN_SIZE 50
+#define OCEAN_SIZE 100
 #define MREQUIN 10
 #define MPOISSON 5
 #define PREP 0.1
 #define EMPTY -1
-#define MAX_ANIMALS 500
-#define HUNGER_LIMIT 5
+#define MAX_ANIMALS 1000
+#define HUNGER_LIMIT 10
 
 #define REPPOISSON 1.0         
 #define ATTRSHARK 1.0       
 #define ATTRSHARK_CLOSEST 2.0 
 #define VISIBILITY_RANGE 20 
-#define CURRENT_FORCE_X 0.1f // Intensité du courant en X
-#define CURRENT_FORCE_Y 0.0f // Intensité du courant en Y
-#define EPSILON 1e-6
 
 typedef struct {
     int type; // 0 pour poisson, 1 pour requin
@@ -34,36 +31,27 @@ typedef struct {
 
 Animal ocean[MAX_ANIMALS];
 
-void initializeLocalOcean(Animal* local_ocean, int* local_count, int start_x, int start_y, int subdomain_size, int world_rank, int world_size) {
-    srand((unsigned int)(time(NULL)) ^ (world_rank + 1));
-    *local_count = 0;
-
-    int animals_per_process = MAX_ANIMALS / world_size;
-    if (world_rank == world_size - 1) {
-        animals_per_process += MAX_ANIMALS % world_size;
-    }
-
-    for (int i = 0; i < animals_per_process; i++) {
-        local_ocean[i].type = rand() % 2;
-        local_ocean[i].x = start_x + (rand() % subdomain_size);
-        local_ocean[i].y = start_y + (rand() % subdomain_size);
-        local_ocean[i].vx = local_ocean[i].vy = 0;
-        local_ocean[i].ax = local_ocean[i].ay = 0;
-        local_ocean[i].hunger = 0;
-        (*local_count)++;
+void initializeOcean() {
+    srand(time(NULL));
+    for (int i = 0; i < MAX_ANIMALS; i++) {
+        ocean[i].type = rand() % 2;
+        ocean[i].x = rand() % OCEAN_SIZE;
+        ocean[i].y = rand() % OCEAN_SIZE;
+        ocean[i].vx = ocean[i].vy = 0;
+        ocean[i].ax = ocean[i].ay = 0;
+        ocean[i].hunger = 0;
     }
 }
 
-void updateForces(Animal* a, Animal* local_ocean, int local_count) {
-    float force_x = CURRENT_FORCE_X;
-    float force_y = CURRENT_FORCE_Y;
+void updateForces(Animal* a) {
+    float force_x = 0.0, force_y = 0.0;
 
-    for (int i = 0; i < local_count; i++) {
-        if (a == &local_ocean[i]) continue;
-        float distance = sqrt(pow(local_ocean[i].x - a->x, 2) + pow(local_ocean[i].y - a->y, 2));
-        if (distance < EPSILON) continue;
-        
-        if (a->type == 1 && local_ocean[i].type == 0) {
+    force_x += 0.1; 
+
+    for (int i = 0; i < MAX_ANIMALS; i++) {
+        float distance = sqrt(pow(ocean[i].x - a->x, 2) + pow(ocean[i].y - a->y, 2));
+
+        if (a->type == 1 && ocean[i].type == 0) {
             if (distance < VISIBILITY_RANGE) {
                 force_x += ATTRSHARK_CLOSEST / pow(distance, 2);
                 force_y += ATTRSHARK_CLOSEST / pow(distance, 2);
@@ -73,7 +61,7 @@ void updateForces(Animal* a, Animal* local_ocean, int local_count) {
             }
         }
 
-        if (a->type == 0 && local_ocean[i].type == 1) {
+        if (a->type == 0 && ocean[i].type == 1) {
             force_x -= REPPOISSON / distance;
             force_y -= REPPOISSON / distance;
         }
@@ -83,235 +71,124 @@ void updateForces(Animal* a, Animal* local_ocean, int local_count) {
     a->ay = force_y / (a->type == 0 ? MPOISSON : MREQUIN);
 }
 
-void updateLocalForces(Animal* local_ocean, int local_count) {
-    for (int i = 0; i < local_count; i++) {
-        updateForces(&local_ocean[i], local_ocean, local_count);
-    }
-}
 
-void handleLocalCollisionsAndReproduction(Animal* local_ocean, int* local_count) {
-    for (int i = 0; i < *local_count; i++) {
-        for (int j = i + 1; j < *local_count; j++) {
-            float distance = sqrt(pow(local_ocean[i].x - local_ocean[j].x, 2) + 
-                                  pow(local_ocean[i].y - local_ocean[j].y, 2));            
+void handleCollisionsAndReproduction() {
+    for (int i = 0; i < MAX_ANIMALS; i++) {
+        for (int j = i + 1; j < MAX_ANIMALS; j++) {
+            float distance = sqrt(pow(ocean[i].x - ocean[j].x, 2) + pow(ocean[i].y - ocean[j].y, 2));
             if (distance < 1.0) { 
-                if (local_ocean[i].type == local_ocean[j].type) {
-                    float temp_vx = local_ocean[i].vx;
-                    float temp_vy = local_ocean[i].vy;
-                    local_ocean[i].vx = local_ocean[j].vx;
-                    local_ocean[i].vy = local_ocean[j].vy;
-                    local_ocean[j].vx = temp_vx;
-                    local_ocean[j].vy = temp_vy;
+                if (ocean[i].type == ocean[j].type) {
+                    float temp_vx = ocean[i].vx;
+                    float temp_vy = ocean[i].vy;
+                    ocean[i].vx = ocean[j].vx;
+                    ocean[i].vy = ocean[j].vy;
+                    ocean[j].vx = temp_vx;
+                    ocean[j].vy = temp_vy;
                 }
                 else {
                     // Shark eats fish
-                   if (local_ocean[i].type == 1 && local_ocean[j].type == 0) {
-                        local_ocean[j].type = EMPTY; 
-                        local_ocean[i].hunger = 0; 
+                    if (ocean[i].type == 1) {
+                        ocean[j].type = EMPTY; 
+                        ocean[i].hunger = 0; 
                     }
-                    else if (local_ocean[i].type == 0 && local_ocean[j].type == 1) {
-                        local_ocean[i].type = EMPTY; 
-                        local_ocean[j].hunger = 0; 
+                    else {
+                        ocean[i].type = EMPTY; 
+                        ocean[j].hunger = 0; 
                     }
                 }
             }
         }
     }
 
-    for (int i = 0; i < *local_count; i++) {
-        if (local_ocean[i].type != EMPTY) {
-            if ((double)rand() / RAND_MAX < PREP) {
-                for (int k = 0; k < *local_count; k++) {
-                    if (local_ocean[k].type == EMPTY) {
-                        local_ocean[k].type = local_ocean[i].type;
-                        local_ocean[k].x = local_ocean[i].x + ((rand() % 3) - 1);
-                        local_ocean[k].y = local_ocean[i].y + ((rand() % 3) - 1);
-                        local_ocean[k].vx = local_ocean[k].vy = 0;
-                        local_ocean[k].ax = local_ocean[k].ay = 0;
-                        local_ocean[k].hunger = 0;
+    for (int i = 0; i < MAX_ANIMALS; i++) {
+        if (ocean[i].type != EMPTY) {
+            if (rand() < PREP * RAND_MAX) {
+                for (int k = 0; k < MAX_ANIMALS; k++) {
+                    if (ocean[k].type == EMPTY) {
+                        ocean[k].type = ocean[i].type;
+                        ocean[k].x = ocean[i].x + ((rand() % 3) - 1);
+                        ocean[k].y = ocean[i].y + ((rand() % 3) - 1);
+                        ocean[k].vx = ocean[k].vy = 0;
+                        ocean[k].ax = ocean[k].ay = 0;
+                        ocean[k].hunger = 0;
                         break;
                     }
                 }
             }
 
-            if (local_ocean[i].type == 1) {
-                local_ocean[i].hunger++;
-                if (local_ocean[i].hunger > HUNGER_LIMIT) {
-                    local_ocean[i].type = EMPTY; 
+            if (ocean[i].type == 1) {
+                ocean[i].hunger++;
+                if (ocean[i].hunger > HUNGER_LIMIT) {
+                    ocean[i].type = EMPTY; 
                 }
             }
         }
     }
 }
 
-
-void updatePosition(Animal* a, float timeStep, int oceanSize) {
+void updatePosition(Animal* a, float timeStep) {
     a->vx += a->ax * timeStep;
     a->vy += a->ay * timeStep;
-
     a->x += a->vx * timeStep;
     a->y += a->vy * timeStep;
-
-    if (a->x >= oceanSize) a->x -= oceanSize;
-    if (a->x < 0) a->x += oceanSize;
-    if (a->y >= oceanSize) a->y -= oceanSize;
-    if (a->y < 0) a->y += oceanSize;
 }
 
-void processReceivedAnimals(Animal* local_ocean, Animal* buffer, int numAnimals, int* local_count, int world_size) {
-    int max_animals_per_process = MAX_ANIMALS / world_size;
+void processReceivedAnimals(Animal* buffer, int numAnimals) {
     for (int i = 0; i < numAnimals; i++) {
-        if (*local_count < max_animals_per_process) {
-            local_ocean[*local_count] = buffer[i];
-            (*local_count)++;
-        } else {
-            int replace_index = rand() % max_animals_per_process;
-            local_ocean[replace_index] = buffer[i];
+        for (int j = 0; j < MAX_ANIMALS; j++) {
+            if (ocean[j].type == EMPTY) {
+                ocean[j] = buffer[i];
+                break;
+            }
         }
     }
 }
 
-void printOcean(Animal* local_ocean, int local_count, int oceanSize, int world_rank, int world_size) {
-    Animal* all_ocean = NULL;
-
-    if (world_rank == 0) {
-        all_ocean = (Animal*)malloc(MAX_ANIMALS * sizeof(Animal));
-        if (all_ocean == NULL) {
-            MPI_Abort(MPI_COMM_WORLD, 1);
-        }
+void printOcean(Animal ocean[], int oceanSize) {
+    printf("  ");
+    for (int j = 0; j < oceanSize; ++j) {
+        printf("%2d", j);
     }
+    printf("\n");
 
-    int gather_result = MPI_Gather(local_ocean, local_count * sizeof(Animal), MPI_BYTE,
-                                   all_ocean, local_count * sizeof(Animal), MPI_BYTE,
-                                   0, MPI_COMM_WORLD);
+    for (int i = 0; i < oceanSize; ++i) {
+        printf("%2d", i);
 
-    if (world_rank == 0) {
-        char display[oceanSize][oceanSize];
-        memset(display, '.', sizeof(display)); 
+        for (int j = 0; j < oceanSize; ++j) {
+            char displayChar = '.'; 
 
-
-        for (int i = 0; i < MAX_ANIMALS; i++) {
-            if (all_ocean[i].type != EMPTY) {
-                int x = (int)all_ocean[i].x;
-                int y = (int)all_ocean[i].y;
-                if (x >= 0 && x < oceanSize && y >= 0 && y < oceanSize) {
-                    display[y][x] = (all_ocean[i].type == 0) ? 'P' : 'R';
+            for (int k = 0; k < MAX_ANIMALS; ++k) {
+                if (ocean[k].type != EMPTY && (int)ocean[k].x == j && (int)ocean[k].y == i) {
+                    displayChar = (ocean[k].type == 0) ? 'P' : 'R';  // 'P' for fish, 'R' for shark
+                    break;
                 }
             }
+            printf(" %c", displayChar);
         }
-
-        for (int i = 0; i < oceanSize; i++) {
-            for (int j = 0; j < oceanSize; j++) {
-                printf(" %c", display[i][j]);
-            }
-            printf("\n");
-        }
-
-        free(all_ocean);
-
-        printf("\nPress Enter to continue to the next round...\n");
-    }
-}
-
-int exchangeAnimals(int world_rank, int world_size, Animal* buffer, int count, Animal* local_ocean, int* local_count) {
-    MPI_Status status;
-    int numAnimalsReceived = 0;
-
-    int next_rank = (world_rank + 1) % world_size;
-    int prev_rank = (world_rank == 0) ? world_size - 1 : world_rank - 1;
-
-    Animal* recv_buffer = new Animal[numAnimalsReceived];
-
-    for (int i = 0; i < numAnimalsReceived; i++) {
-        if (*local_count < MAX_ANIMALS / world_size) {
-            local_ocean[*local_count] = recv_buffer[i];
-            (*local_count)++;
-        }
+        printf("\n");
     }
 
-    delete[] recv_buffer;
-    return numAnimalsReceived;
+     printf("\nPress Enter to continue to the next round...\n");
+     getchar(); 
 }
 
-int prepareExchange(Animal* local_ocean, Animal* buffer, int* local_count, int start_x, int start_y, int subdomain_size) {
-    int count = 0;
-    
-    for (int i = 0; i < *local_count; i++) {
-        int end_x = start_x + subdomain_size;
-        int end_y = start_y + subdomain_size;
-        
-        if (local_ocean[i].x < start_x || local_ocean[i].x >= end_x ||
-            local_ocean[i].y < start_y || local_ocean[i].y >= end_y) {
-            buffer[count] = local_ocean[i];
-            count++;
-            
-            local_ocean[i].type = EMPTY;
-        }
-    }
-
-    *local_count -= count;
-    
-    return count;
-}
-
-
-int main(int argc, char** argv) {
-
+int main() {
     typedef std::chrono::high_resolution_clock Time;
     typedef std::chrono::milliseconds ms;
     typedef std::chrono::duration<float> fsec;
     auto t0 = Time::now();
-    int world_size, world_rank;
+    initializeOcean();
 
-    int subdomain_size = OCEAN_SIZE / sqrt(world_size);
-    int start_x = (world_rank % (int)sqrt(world_size)) * subdomain_size;
-    int start_y = (world_rank / (int)sqrt(world_size)) * subdomain_size;
-
-    Animal local_ocean[MAX_ANIMALS / world_size]; 
-    int local_count = 0;
-
-    initializeLocalOcean(local_ocean, &local_count, start_x, start_y, subdomain_size, world_rank, world_size);
-        
     float timeStep = 1.0;
-    int shark_count = 0, fish_count = 0;
-
 
     for (int step = 0; step < 1000; step++) {
-        printOcean(local_ocean, local_count, OCEAN_SIZE, world_rank, world_size);
-        updateLocalForces(local_ocean, local_count);
-        for (int i = 0; i < local_count; i++) {
-            updatePosition(&local_ocean[i], timeStep, OCEAN_SIZE);
-        } 
-        
-        handleLocalCollisionsAndReproduction(local_ocean, &local_count);
-
-        Animal buffer[MAX_ANIMALS]; 
-        int count = prepareExchange(local_ocean, buffer, &local_count, start_x, start_y, subdomain_size);
-        
-        int numAnimalsReceived = exchangeAnimals(world_rank, world_size, buffer, count, local_ocean, &local_count);
-        processReceivedAnimals(local_ocean, buffer, numAnimalsReceived, &local_count, world_size); 
-
-        shark_count = 0;
-        fish_count = 0;
-
-        for (int i = 0; i < local_count; i++) {
-            if (local_ocean[i].type == 1) shark_count++;
-            if (local_ocean[i].type == 0) fish_count++;
+        for (int i = 0; i < MAX_ANIMALS; i++) {
+            updateForces(&ocean[i]);
+            updatePosition(&ocean[i], timeStep);
         }
 
-        if (shark_count == 0) {
-            
-                printf("End of simulation: Fish win! No more sharks left.\n");
-            
-            break;
-        }
-    
-        if (fish_count == 0) {
-            
-                printf("End of simulation: Sharks win! No more fish left.\n");
-            
-            break;
-        }
+        handleCollisionsAndReproduction();
+        printOcean();
     }
     auto t1 = Time::now();
     fsec fs = t1 - t0;
